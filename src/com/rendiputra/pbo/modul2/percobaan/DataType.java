4fe74be6da66638a8b166396f94eacb350adf4cf
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2021-10-13 02:57:00 +0700

    modul2: add file DataType.java

---
+++ src/com/rendiputra/pbo/modul2/percobaan/DataType.java
@@ -0,0 +1,27 @@
+package com.rendiputra.pbo.modul2.percobaan;
+
+public class DataType {
+    public static void main(String[] args) {
+
+        // tipe data primitif
+        long data1 = 22123213;
+        int data2 = 1231232;
+        short data3 = 123;
+        byte data4 = 123;
+
+        float data5 = (float) 1.123;
+        double data6 = 4.213;
+        char data7 = 'Y';
+        boolean data8 = true;
+
+        System.out.println("Nilai long  : " + data1);
+        System.out.println("Nilai Int   : " + data2);
+        System.out.println("Nilai Short : " + data3);
+        System.out.println("Nilai Byte  : " + data4);
+        System.out.println("Nilai Double: " + data5 );
+        System.out.println("Nilai Float : " + data6);
+        System.out.println("Nilai Char  : " + data7);
+        System.out.println("Nilai Boolean   :" + data8);
+
+    }
+}
