3ff432e2390d716d29e91cecbbe6199a8d33186c
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2021-10-13 02:56:28 +0700

    modul2: add file Aritmatika.java

---
+++ src/com/rendiputra/pbo/modul2/percobaan/Aritmatika.java
@@ -0,0 +1,30 @@
+package com.rendiputra.pbo.modul2.percobaan;
+
+public class Aritmatika {
+    public static void main(String[] args) {
+
+        // contoh oprasi bilangan bulat
+        System.out.println("Operasi aritmatika " + " pada tipe integer");
+
+        int a = 2 + 2;
+        int b = a - 2;
+        int c = a * 3;
+        int d = c / 2;
+        int e = -2;
+
+        System.out.println("Nilai a: " + a);
+        System.out.println("Nilai b: " + b);
+        System.out.println("Nilai c: " + c);
+        System.out.println("Nilai d: " + d);
+        System.out.println("Nilai e: " + e);
+
+        // bilangan desimal
+        System.out.println("Operasi aritmatika " + " pada tipe floating-point");
+
+        double fa = 2.2 + 2.5;
+        double fb = a - 2.3;
+        double fc = a * 3.7;
+        double fd = c / 2.1;
+        double fe = -2.2;
+    }
+}
